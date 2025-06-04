1ac0aab62fd87eecb4af858f79dc0b0c97fa314a
Bharat8327 <bp609213@gmail.com>
2025-06-04 09:02:50 +0530

    add more button add when view cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import { BsCart4 } from "react-icons/bs";
+import React from 'react';
+import { BsCart4 } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function NavBar() {
-  const cartItem = useSelector(state => state.cartReducer.cart);
+  const location = useLocation();
+
+  const cartItem = useSelector((state) => state.cartReducer.cart);
   let count = 0;
-  cartItem.forEach(element => count += element.quantity);
+  cartItem.forEach((element) => (count += element.quantity));
 
   const navigate = useNavigate();
 
   return (
     <nav>
-      <div className='bg-emerald-400 flex justify-between items-center fixed top-0 w-full'  style={{ background: "linear-gradient(to bottom, #e55d87, #5fc3e4)" }}>
-        <h1 className='text-3xl font-semibold text-amber-300 py-1 pl-2 animate-bounce'>
-          <span className='text-black'>B</span>Patel....
+      <div
+        className="bg-emerald-400 flex justify-between items-center fixed top-0 w-full"
+        style={{ background: 'linear-gradient(to bottom, #e55d87, #5fc3e4)' }}
+      >
+        <h1 className="text-3xl font-semibold text-amber-300 py-1 pl-2 animate-bounce">
+          <span className="text-black">B</span>Patel....
         </h1>
         <div>
-          <div className='flex items-center gap-3  pr-1 text-white'>
-            <div className='text-xl'>
-              <Link to='/'>Add More</Link>
-            </div>
-            <span className='flex '>{count != 0 ? <h2><sup>{count}</sup></h2> : <p></p>}
-              <BsCart4 className='text-2xl ' onClick={() => navigate('/cart')} style={{ cursor: 'pointer' }} />
+          <div className="flex items-center gap-3  pr-1 text-white">
+            {location.pathname === '/cart' && (
+              <div className="text-xl">
+                <Link to="/">Add More</Link>
+              </div>
+            )}
+            <span className="flex ">
+              {count != 0 ? (
+                <h2>
+                  <sup>{count}</sup>
+                </h2>
+              ) : (
+                <p></p>
+              )}
+              <BsCart4
+                className="text-2xl "
+                onClick={() => navigate('/cart')}
+                style={{ cursor: 'pointer' }}
+              />
             </span>
-
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
